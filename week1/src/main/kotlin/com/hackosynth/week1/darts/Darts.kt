4a15c2512e467f096a9aeff82fc73a23d5faaa6c
Robert Möttus Löfgren <robert@mottus.se>
2020-03-08 22:18:11 +0100

    Update Darts.kt

--- week1/src/main/kotlin/com/hackosynth/week1/darts/Darts.kt
+++ week1/src/main/kotlin/com/hackosynth/week1/darts/Darts.kt
@@ -7,5 +7,15 @@ object Darts {
      * @param y - the Y coordinate
      * @return the score the given coordinates would yield
      */
-    fun score(x: Number, y: Number): Int = TODO()
+    fun score(x: Number, y: Number): Int {
+        val radius = sqrt(square(x) + square(y))
+        return when {
+            radius <= 1.0 -> 10
+            radius <= 5 -> 5
+            radius <= 10 -> 1
+            else -> 0
+        }
+    }
+
+    fun square(x: Number) : Double = x.toDouble() * x.toDouble()
 }
