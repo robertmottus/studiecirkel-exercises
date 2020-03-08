7681fc6104e74d5ba8531b89666c1514d7748822
Robert Möttus Löfgren <robert@mottus.se>
2020-03-08 22:12:35 +0100

    Implementerat Acronym

--- week1/src/main/kotlin/com/hackosynth/week1/acronym/Acronym.kt
+++ week1/src/main/kotlin/com/hackosynth/week1/acronym/Acronym.kt
@@ -5,5 +5,12 @@ object Acronym {
      * Generates an acronym form a given sentence
      * @return the acronym for the sentence
      */
-    fun generate(phrase: String): String = TODO()
+    fun generate(phrase: String): String {
+        val words = phrase
+            .replace("-", " ")
+            .replace("_", " ")
+            .replace("'", "")
+            .split("\\s+".toRegex())
+        return words.fold("", {acc, w -> acc + w.first().toUpperCase()})
+    }
 }
