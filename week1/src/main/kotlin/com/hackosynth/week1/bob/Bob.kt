a38d6698c36724403b3b10005edfd3dfb7236165
Robert Möttus Löfgren <robert@mottus.se>
2020-03-08 22:16:42 +0100

    Implementerat Bob

--- week1/src/main/kotlin/com/hackosynth/week1/bob/Bob.kt
+++ week1/src/main/kotlin/com/hackosynth/week1/bob/Bob.kt
@@ -6,5 +6,22 @@ object Bob {
      * @param input - the sentence spoken to bob
      * @return bob's response to the spoken sentence
      */
-    fun hey(input: String): String = TODO()
+    fun hey(input: String): String {
+        val trimmedInput = input.trim()
+        val yelling = isStrictUpperCase(trimmedInput)
+        val asking = trimmedInput.endsWith("?")
+        val silent = trimmedInput.isEmpty()
+
+        return when {
+            silent -> "Fine. Be that way!"
+            asking && yelling -> "Calm down, I know what I'm doing!"
+            asking -> "Sure."
+            yelling -> "Whoa, chill out!"
+            else -> "Whatever."
+        }
+    }
+
+    fun isStrictUpperCase(str: String) : Boolean =
+        str.toUpperCase() == str &&
+                str.toLowerCase() != str
 }
