23ec75595e4f09e20afa6b2c4266e1cd7b3d1c8a
robertmottus <Kylskp1>
2020-03-08 20:42:29 +0100

    Implemented SpaceAge

--- week2/src/main/kotlin/com/hackosynth/week2/spaceage/SpaceAge.kt
+++ week2/src/main/kotlin/com/hackosynth/week2/spaceage/SpaceAge.kt
@@ -1,12 +1,17 @@
 package com.hackosynth.week2.spaceage
 
+import kotlin.math.round
+
 class SpaceAge(val ageSeconds: Long) {
-    fun onEarth(): Double = TODO()
-    fun onMercury(): Double = TODO()
-    fun onVenus(): Double = TODO()
-    fun onMars(): Double = TODO()
-    fun onJupiter(): Double = TODO()
-    fun onSaturn(): Double = TODO()
-    fun onUranus(): Double = TODO()
-    fun onNeptune(): Double = TODO()
+    fun round2dec(n: Double):Double = round(n*100)/100
+    val daysInYear = 365.25
+
+    fun onEarth(): Double = round2dec(ageSeconds.toDouble() / 31557600)
+    fun onMercury(): Double = round2dec(onEarth() / 0.2408467)
+    fun onVenus(): Double = round2dec(onEarth() / 0.61519726 )
+    fun onMars(): Double = round2dec(onEarth() /  1.8808158)
+    fun onJupiter(): Double = round2dec(onEarth() / 11.862615 )
+    fun onSaturn(): Double = round2dec(onEarth() / 29.447498)
+    fun onUranus(): Double = round2dec(onEarth() / 84.016846)
+    fun onNeptune(): Double = round2dec(onEarth() / 164.79132)
 }
