a439008f001d3f7fc73be1a7478006dc63a6f586
Robert Möttus Löfgren <robert@mottus.se>
2020-03-08 22:36:33 +0100

    Update ScrabbleScore.kt

--- week2/src/main/kotlin/com/hackosynth/week2/scrabblescore/ScrabbleScore.kt
+++ week2/src/main/kotlin/com/hackosynth/week2/scrabblescore/ScrabbleScore.kt
@@ -2,5 +2,14 @@ package com.hackosynth.week2.scrabblescore
 
 object ScrabbleScore {
 
-    fun scoreWord(word: String): Int = TODO()
+     word.toUpperCase().fold(0, {acc,ch -> acc+ when {
+         "AEIOULNRST".contains(ch) -> 1
+         "DG".contains(ch) -> 2
+         "BCMP".contains(ch) -> 3
+         "FHVWY".contains(ch) -> 4
+         "K".contains(ch) -> 5
+         "JX".contains(ch) -> 8
+         "QZ".contains(ch) -> 10
+         else -> 0
+     } })
 }
