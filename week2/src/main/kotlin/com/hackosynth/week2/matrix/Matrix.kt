96ff56f9298fb8060adc366a1df6ae6c592c7859
Robert Löfgren <robert@mottus.se>
2020-03-12 22:13:21 +0100

    Implementerade vecka 2: Matrix

--- week2/src/main/kotlin/com/hackosynth/week2/matrix/Matrix.kt
+++ week2/src/main/kotlin/com/hackosynth/week2/matrix/Matrix.kt
@@ -1,6 +1,23 @@
 package com.hackosynth.week2.matrix
 
-class Matrix(private val matrixAsString: String) {
-    fun column(colNr: Int): List<Int> = TODO()
-    fun row(rowNr: Int): List<Int> = TODO()
+class Matrix(private val matrixAsStringRaw: String) {
+//    val matrixAsString = matrixAsStringRaw.trimIndent().replace("\\s+".toRegex(), " ")
+    val matrixAsString = matrixAsStringRaw.trimIndent().replace(" +".toRegex(), " ")
+    val rows = matrixAsString.split("\n").size
+    val cols = matrixAsString.split("\n")[0].split(" ").size
+
+    var matrix = Array(rows, {IntArray(cols)})
+
+    init{
+        for(rowNr in 1..rows) {
+            val rowAsString = matrixAsString.split("\n")[rowNr - 1].trim() // <- varför behövs denna?
+            for(colNr in 1..cols) {
+                val element = rowAsString.split(" ")[colNr - 1].toInt()
+                matrix[rowNr-1][colNr-1] = element
+            }
+        }
+    }
+
+    fun column(colNr: Int): List<Int> = matrix.map{row -> row[colNr-1]}
+    fun row(rowNr: Int): List<Int> = matrix[rowNr-1].toList()
 }
