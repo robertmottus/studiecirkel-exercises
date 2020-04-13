4f1009edc40460e2f34b16afa4d829cdaf924bc3
Robert Löfgren <robert@mottus.se>
2020-04-13 19:18:39 +0200

    Week 3. Upg 2 - Isogram Implementerad.

--- week3/src/main/kotlin/com/hackosynth/week3/isogram/Isogram.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/isogram/Isogram.kt
@@ -1,5 +1,16 @@
 package com.hackosynth.week3.isogram
 
 object Isogram {
-    fun isIsogram(input: String): Boolean = TODO()
+    fun isIsogram(input: String): Boolean {
+        val chars = mutableSetOf<Char>()
+        for (char in input) {
+            if(!char.isLetter()) continue
+            val c = char.toLowerCase()
+            if(chars.contains(c))
+                return false
+            else
+                chars.add(c)
+        }
+        return true;
+    }
 }
