e2816c3466ec51149987f4f8978d5501ca746ba6
Robert Löfgren <robert@mottus.se>
2020-04-13 18:56:45 +0200

    Week 3. Upg 1 - Natural numbers Implementerad.

--- week3/src/main/kotlin/com/hackosynth/week3/naturalnumber/NaturalNumber.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/naturalnumber/NaturalNumber.kt
@@ -1,9 +1,21 @@
 package com.hackosynth.week3.naturalnumber
 
+import com.hackosynth.week3.naturalnumber.Classification.*
+
 enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
 object NaturalNumber {
-    fun classify(naturalNumber: Int): Classification = TODO()
+    fun classify(naturalNumber: Int): Classification {
+        require(naturalNumber > 0)
+        fun aliquotSum (n: Int):Int = (1..n/2)
+            .filter {n%it==0}
+            .sum()
+        return when {
+            aliquotSum(naturalNumber) < naturalNumber -> DEFICIENT
+            aliquotSum(naturalNumber) > naturalNumber -> ABUNDANT
+            else -> PERFECT
+        }
+    }
 }
