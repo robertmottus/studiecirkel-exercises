703d5659d5314edc22f86ea59ea53f1495a2b2f9
Robert Löfgren <robert@mottus.se>
2020-04-13 22:00:14 +0200

    Week 3. Upg 3 - Diamond Implementerad.

--- week3/src/main/kotlin/com/hackosynth/week3/diamond/Diamond.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/diamond/Diamond.kt
@@ -1,5 +1,29 @@
 package com.hackosynth.week3.diamond
 
+import java.lang.Math.abs
+
 object Diamond {
-    fun printToList(letter: Char): List<String> = TODO()
+    fun printToList(letter: Char): List<String> {
+        require(letter in 'A'..'Z')
+        val size = 1 + (letter - 'A') * 2 // width and height
+        fun distanceFromCenter(rowIndex: Int) = abs(rowIndex - (size-1)/2)
+        fun letter(rowIndex: Int) = letter - distanceFromCenter(rowIndex)
+        var board:MutableList<String> = MutableList(size) {""}
+        for(rowIndex in (0..size-1)) {
+//            board[rowIndex].padEnd(size)
+            val centerIndex = (size-1)/2
+            val rowLetter = letter(rowIndex)
+            var row = CharArray(size, {' '})
+            if(rowIndex==0 || rowIndex==size-1) {
+                row[centerIndex] = rowLetter
+            } else {
+                val colIndex1 = distanceFromCenter(rowIndex)
+                row[colIndex1] = rowLetter
+                val colIndex2 = size-1-distanceFromCenter(rowIndex)
+                row[colIndex2] = rowLetter
+            }
+            board[rowIndex] = row.joinToString("")
+        }
+        return board
+    }
 }
